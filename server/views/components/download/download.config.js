ce52372f56084564c8be8a05d7b9a3057f0b1150
Gareth Estchild <g.estchild@wellcome.ac.uk>
2017-08-08 16:25:39 +0100

    Started work on download component

---
+++ server/views/components/download/download.config.js
@@ -0,0 +1,23 @@
+export const name = 'download';
+export const handle = 'download';
+export const status = 'wip';
+export const context = {
+  model: {
+    imgLink: 'https://iiif.wellcomecollection.org/image/V0023074.jpg/full/WIDTH,/0/default.jpg',
+    sizes: [
+      {
+        text: 'small',
+        width: 800
+      },
+      {
+        text: 'medium',
+        width: 1300
+      },
+      {
+        text: 'large',
+        width: 2048
+      }
+    ]
+  }
+};
+
